f411dc1ce8f279196d8d6c4d0cd16f7f03db2bb4
Shailesh Nayak <30549931+shailesh-nyk@users.noreply.github.com>
2019-10-06 03:08:41 -0700

    Updating DB instance in config file

--- Grubhub/BackEnd/db_connection.js
+++ Grubhub/BackEnd/db_connection.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2');
 // create the connection to database
 var connection = mysql.createConnection({
     // connectionLimit: 10,
-    host: 'localhost',
+    host: 'grubhub-db.cmmdmyrzolij.us-east-2.rds.amazonaws.com',
     port: '3306',
-    user: 'root',
-    password: 'Ccompiler7!',
+    user: 'snayakk',
+    password: 'grubqwerty',
     database: 'grubhub',
     dateStrings: true
 });
