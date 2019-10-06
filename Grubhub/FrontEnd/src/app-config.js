88927b12a4faf7d10669048f3fe0d2dec4a1ed88
Shailesh Nayak <30549931+shailesh-nyk@users.noreply.github.com>
2019-10-06 03:11:10 -0700

    Updating EC2 server instance in config

--- Grubhub/FrontEnd/src/app-config.js
+++ Grubhub/FrontEnd/src/app-config.js
@@ -1,6 +1,6 @@
 const config = {
-    api_host : 'http://localhost:8000/api',
-    base: 'http://localhost:8000/'
+    api_host : 'http://ec2-3-19-223-81.us-east-2.compute.amazonaws.com:3000/api',
+    base: 'http://ec2-3-19-223-81.us-east-2.compute.amazonaws.com:3000/'
 }
 
 export default config;
